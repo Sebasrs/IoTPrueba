59e1ad08e86e21c8f917b1b9c6a4959d5f4dd939
Sebastian Rivera Soto <SebastianRS72@gmail.com>
2018-07-27 22:02:19 -0600

    API

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', function(req, res){
+  res.send("Tech Abuse API");
+});
+
 app.listen(PORT, function(){
   console.log("Listening on PORT: "+PORT);
 })
