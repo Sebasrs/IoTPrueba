47ea6766387f0fd202b80d203d1144dc6c2c57da
Sebastian Rivera Soto <SebastianRS72@gmail.com>
2018-07-27 22:31:07 -0600

    Agrego views

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
+var path    = require("path");
+var cons = require('consolidate');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.engine('html', cons.swig)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', function(req, res){
-  res.send("Tech Abuse API");
+  res.render('index');
 });
 
 app.listen(PORT, function(){
